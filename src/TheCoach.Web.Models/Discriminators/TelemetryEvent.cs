bdac8c5edcdf48d261bc8687a5a5620ad1fde988
Αλέξανδρος Κρητικός <akritikos@outlook.com>
2018-09-05 11:21:40 +0300

    Adds enumerator for DataTracking

---
+++ src/TheCoach.Web.Models/Discriminators/TelemetryEvent.cs
@@ -0,0 +1,10 @@
+namespace TheCoach.Web.Models.Discriminators
+{
+	public enum TelemetryEvent
+	{
+		LocalPlaylistCreated = 0,
+		PlaylistDownloaded = 1,
+		PlaylistStarted = 2,
+		PlaylistStopped = 3,
+	}
+}
