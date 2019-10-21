9247e5fadecafe1bd5661c587d628ed548d513d5
xdinos <xdinos@gmail.com>
2019-10-21 12:55:54 +0300

    - change data type

--- src/TheCoach.Web.Models/Binding/PurchaseLessonBindingModel.cs
+++ src/TheCoach.Web.Models/Binding/PurchaseLessonBindingModel.cs
@@ -4,7 +4,7 @@ namespace TheCoach.Web.Models.Binding
 {
 	public class PurchaseLessonBindingModel
 	{
-		public int LessonId { get; set; }
+		public long LessonId { get; set; }
 		public string StoreProductId { get; set; }
 		public string PurchaseToken { get; set; }
 		public PurchaseState State { get; set; }
