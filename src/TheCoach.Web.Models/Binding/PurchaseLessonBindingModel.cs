8f557a99659cab2b505f73db580992c69a093048
xdinos <xdinos@gmail.com>
2019-10-22 10:30:34 +0300

    update purchase binding data model

--- src/TheCoach.Web.Models/Binding/PurchaseLessonBindingModel.cs
+++ src/TheCoach.Web.Models/Binding/PurchaseLessonBindingModel.cs
@@ -5,6 +5,8 @@ namespace TheCoach.Web.Models.Binding
 	public class PurchaseLessonBindingModel
 	{
 		public long LessonId { get; set; }
+		public string OrderId { get; set; }
+		public string DevicePlatform { get; set; }
 		public string StoreProductId { get; set; }
 		public string PurchaseToken { get; set; }
 		public PurchaseState State { get; set; }
