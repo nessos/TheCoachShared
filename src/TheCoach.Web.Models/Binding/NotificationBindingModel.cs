570778d592c89420ec22a7373a7c0a29678e4c47
xdinos <xdinos@gmail.com>
2020-03-23 20:02:34 +0200

    - add missing file

---
+++ src/TheCoach.Web.Models/Binding/NotificationBindingModel.cs
@@ -0,0 +1,15 @@
+namespace TheCoach.Web.Models.Binding
+{
+	public class NotificationBindingModel
+	{
+		public string UserId { get; set; }
+
+		public string Type { get; set; }
+
+		public string Display { get; set; }
+
+		public string Data { get; set; }
+
+		public string Title { get; set; }
+	}
+}
