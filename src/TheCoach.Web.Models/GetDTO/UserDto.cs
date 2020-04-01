91034c68f77befed2c32930e5741e78d468ca472
Dimitris Tsapetis <dimtsap@hotmail.com>
2020-04-01 11:51:16 +0300

    Adds Get UserDto

---
+++ src/TheCoach.Web.Models/GetDTO/UserDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCoach.Web.Models.GetDTO
+{
+	public class UserDto
+	{
+		public string Id { get; set; }
+
+		public string Email { get; set; }
+
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+}
