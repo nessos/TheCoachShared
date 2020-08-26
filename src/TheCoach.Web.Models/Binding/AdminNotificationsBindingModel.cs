b4809da8ac3d919e32bef09c61b4f2dd35da149b
Dimitris Tsapetis <dimtsap@hotmail.com>
2020-08-26 16:34:05 +0300

    Adds AdminNotificationsBindingModel

---
+++ src/TheCoach.Web.Models/Binding/AdminNotificationsBindingModel.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCoach.Web.Models.Binding
+{
+	public class AdminNotificationsBindingModel
+	{
+		public List<string> NotificationTokens { get; set; }
+
+		public string Type { get; set; }
+
+		public string Display { get; set; }
+
+		public string Data { get; set; }
+
+		public string Title { get; set; }
+	}
+}
